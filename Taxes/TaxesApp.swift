01c0bd647be3faf19ddf5b814eb0827c4fc6a7fd
Paul <pasha.makey@gmail.com>
2024-03-16 19:30:16 +0300

    Initial Commit

---
+++ Taxes/TaxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxesApp.swift
+//  Taxes
+//
+//  Created by Paul Makey on 16.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TaxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
